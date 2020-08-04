612f7b061967babdcae652d73b6b70bb399b40db
Sean <sean.rich@mac.com>
2020-08-03 21:14:33 -0400

    Initial Commit

---
+++ TableViewGapTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TableViewGapTest
+//
+//  Created by Sean Rich on 8/3/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TableViewGapTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TableViewGapTest
+//
+//  Created by Sean Rich on 8/3/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
